500f5f27cb64095f97a7343ca053169893814973
IsaacAlvaradoMata <isaac.alvarado.mata@est.una.ac.cr>
2025-03-31 20:49:03 -0600

    upd

--- src/main/java/cr/ac/una/unaplanilla2025/controller/TiposPlanillaController.java
+++ src/main/java/cr/ac/una/unaplanilla2025/controller/TiposPlanillaController.java
@@ -4,9 +4,16 @@
  */
 package cr.ac.una.unaplanilla2025.controller;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXCheckbox;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -14,6 +21,33 @@ import javafx.fxml.Initializable;
  * @author josue_5njzopn
  */
 public class TiposPlanillaController extends Controller implements Initializable {
+
+    @FXML
+    private MFXTextField txtId;
+    @FXML
+    private MFXCheckbox chkActivo;
+    @FXML
+    private MFXTextField txtCodigo;
+    @FXML
+    private MFXTextField txtDescripcion;
+    @FXML
+    private MFXTextField txtPlantillasXmes;
+    @FXML
+    private MFXTextField txtEmpleado;
+    @FXML
+    private MFXTextField txtNombre;
+    @FXML
+    private TableView<?> tblvListaEmpleados;
+    @FXML
+    private MFXButton btnNuevo;
+    @FXML
+    private MFXButton btnBuscar;
+    @FXML
+    private MFXButton btnEliminar;
+    @FXML
+    private MFXButton btnGuardar;
+    @FXML
+    private ImageView imgAdd;
 
     /**
      * Initializes the controller class.
@@ -25,6 +59,22 @@ public class TiposPlanillaController extends Controller implements Initializable
 
     @Override
     public void initialize() {
+    }
+
+    @FXML
+    private void onActionBtnNuevo(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnBuscar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnEliminar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnGuardar(ActionEvent event) {
     }
     
 }
